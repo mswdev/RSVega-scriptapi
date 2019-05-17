471db1c22d1d79e8f587909665bacd8e5f3a81ce
Sphiinx <sphiinx33@gmail.com>
2019-05-16 22:44:25 -0600

    Improvments
    - Removed unused imports.
    - Added debug messages.

--- src/org/api/http/bot/stats/StatsOSRS.java
+++ src/org/api/http/bot/stats/StatsOSRS.java
@@ -10,7 +10,6 @@ import org.api.http.wrappers.Request;
 import org.rspeer.runetek.api.component.tab.Skill;
 import org.rspeer.runetek.api.component.tab.Skills;
 import org.rspeer.runetek.api.scene.Players;
-import org.rspeer.ui.Log;
 
 import java.io.IOException;
 
