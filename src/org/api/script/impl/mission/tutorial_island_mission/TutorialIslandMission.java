1749b70d8d073627544cc5ed650dccfef6ff775c
Matt <sphiinx33@gmail.com>
2020-02-04 23:42:30 -0700

    Emergency hotfix for api url

--- src/org/api/script/impl/mission/tutorial_island_mission/TutorialIslandMission.java
+++ src/org/api/script/impl/mission/tutorial_island_mission/TutorialIslandMission.java
@@ -29,7 +29,7 @@ import java.util.Map;
 import java.util.logging.Level;
 
 public class TutorialIslandMission extends Mission {
-    
+
     private final Path createdAccountsPath;
     private final Args args;
     private final Map<String, String> accountData;
